be6f0ecde09a37a57ade872c25a602c5e4cf0907
alex <apryakhin@rzdit.ru>
2024-10-31 16:48:22 +0300

    feat: add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import AirportInputSearch from '@/components/AirportInputSearch';
 import UIContainer from '@/ui/UIContainer';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import LocalizationProvider from '@/providers/LocalizationProvider';
+import dayjs from 'dayjs';
+import { Button } from '@mui/material';
 
 const Page = () => {
   const [from, setFrom] = useState('');
@@ -26,8 +28,10 @@ const Page = () => {
         />
 
         <LocalizationProvider>
-          <DatePicker />
+          <DatePicker minDate={dayjs(new Date())} />
         </LocalizationProvider>
+
+        <Button variant="contained"> Искать </Button>
       </div>
     </UIContainer>
   );
