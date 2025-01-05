ffb30c77ecb66801777fc37fdb48870ee9839473
Aleksey <Alecsey913@mail.ru>
2025-01-05 17:31:45 +0300

    feat: forgot login page)

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '@/components/Login/LoginForm';
+import UIContainer from '@/ui/UIContainer';
+
+const LoginPage = () => {
+  return (
+    <UIContainer>
+      <LoginForm />
+    </UIContainer>
+  );
+};
+
+export default LoginPage;
